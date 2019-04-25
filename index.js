ebad363ee8c88dcbca033d6493687d914c137092
marinakeu <marina_keunecke@hotmail.com>
2019-04-25 18:25:33 -0300

    validator - implementa funções de validação e incrementa testes

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
-function cardValidator (cardNumber) {
-
+function cardValidator (cardNumberString) {
+  let cardNumber = cardNumberString.split('');
+  let sumCardNumber = parseInt(cardNumber.reduce((acc, element) => acc + element));
+  if (cardNumber.length < 14 || sumCardNumber === 0) {
+    return false;
+  } else if (digitValidator(cardNumber)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+  
+function digitValidator (cardNumber) {
+  cardNumber.reverse();
+  let multiply = cardNumber.map(Number).map((element, index) => index % 2 !== 0 ? element * 2 : element);
+  let decrease = multiply.map((element) => element > 9 ? element - 9 : element);
+  let sum = decrease.reduce((acc, element) => acc + element);
+  let result = sum % 10 === 0;
+  return result;
 }
 
 
